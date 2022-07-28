1704e0969e00909ad265846d0f66dfd298f92ecf
ackn14 <levia731.ky@gmail.com>
2022-07-28 12:17:23 +0900

    ログインフォーム作成

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+// Welcome.vueファイル読み込み
+import Welcome from '../views/Welcome'
 
 const routes = [
+  // Welcome.vueファイル読み込み
   {
     path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    name: 'Welcome',
+    component: Welcome
   }
 ]
 
